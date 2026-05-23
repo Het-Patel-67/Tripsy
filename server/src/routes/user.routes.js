7ffd360c2217d91cbe41aaef154b1b070f3a68a4
Developer-Het-67 <patelhet808@gmail.com>
2026-05-23 18:04:44 +0530

    removed refreshAccessToken

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,12 +1,11 @@
 import {Router } from 'express';
-import { loginUser, registerUser, refreshAccessToken, logoutUser, getMe } from '../controllers/user.controller.js';
+import { loginUser, registerUser,  logoutUser, getMe } from '../controllers/user.controller.js';
 import  {verifyJWT} from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.post("/register",registerUser)
 router.post("/login",loginUser)
-router.post("/refresh-token",refreshAccessToken)
 router.get("/me",      verifyJWT, getMe);
 router.post("/logout", verifyJWT, logoutUser);
 export default router;
