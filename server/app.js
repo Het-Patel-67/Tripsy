84628365b79370fb7af48eb4e2eb61dc5042db38
Developer-Het-67 <patelhet808@gmail.com>
2026-05-29 18:03:41 +0530

    ping route added

--- server/app.js
+++ server/app.js
@@ -16,6 +16,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/ping", (req, res) => {
+  res.status(200).send("OK");
+});
 // Routes
 import router from './src/routes/user.routes.js';
 import destiRouter from './src/routes/destination.routes.js';
